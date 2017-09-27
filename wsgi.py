c58f4a54df71bc248a9da9432e59d678abada027
Founsy <founs@founs.eur.adobe.com>
2017-09-27 13:55:48 +0200

    Actions api init
    Add the ‘actions’ API and return a JSON.

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,32 @@
-from flask import Flask
+
+from flask import Flask, jsonify
+
 application = Flask(__name__)
+
+actions = [
+    {
+        'id': 1,
+        'title': u'Assets created',
+        'description': u'A CC Assets has been created.',
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Assets modified',
+        'description': u'A CC Assets has been modified.',
+        'done': False
+    },
+    {
+        'id': 3,
+        'title': u'Assets removed',
+        'description': u'A CC Assets has been removed.',
+        'done': False
+    }
+]
+
+@application.route('/actions', methods=['GET'])
+def get_actions():
+    return jsonify({'actions': actions})
 
 @application.route("/")
 def hello():
